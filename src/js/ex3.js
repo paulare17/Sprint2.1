3b15648ad354d723cc81fac5943dee7e4fd91562
Paula <paulare17@gmail.com>
2025-02-18 16:47:34 +0100

    Exercici finalitzat

--- src/js/ex3.js
+++ src/js/ex3.js
@@ -1,37 +1,91 @@
 'use strict'
 
-// Exercici 1 -NO ACABAT
+// Exercici 1
 
-function activarPrompt() {
-    const numero = Number(prompt("Exercici 1: introdueixi un número"))
-    return numero;
-
-}
-
-
-function processar(nombre, callb) {
-    callb(nombre) 
-} 
-
-const numero = activarPrompt();
-
-processar(numero, function(num) {
-    const result = (num * 2); // Multiplica el nombre per 2
-    document.getElementById('result').innerHTML = result;
+document.getElementById('calculaDoble').addEventListener('click', () =>{
     
-});
+    const num = parseFloat(document.getElementById('numero').value)
+    function processar(numero, callback) {
+        callback(numero);
+    }
+    
+    function mostrarDoble(n) {
+        document.getElementById('result').innerHTML= (n * 2);
+    }
+    
+    processar(num, mostrarDoble);
+    
+})
 
 
 
 //Exercici 2 
 
-// const resultDiv = document.getElementById('result2')
+document.getElementById('calculaSuma').addEventListener('click', () =>{
 
-// function calculadora(num1, num2, callback) {
-//     const num1 = parseInt(document.getElementById('num2').value)
-//     const num2 = parseInt(document.getElementById('num3').value)
-    
-//     callback()
-// }
+const num1 = parseFloat(document.getElementById('num1').value);
+const num2 = parseFloat(document.getElementById('num2').value);
 
-// calculadora(3, 4, () => {num1 + num2})
+function calculadora(a, b, callb
+) {
+   return callb(a, b)
+}
+
+const result = calculadora(num1, num2, (a, b) => a + b)
+document.getElementById('result2').innerHTML = result;
+
+})
+
+//Exercici 3
+
+document.getElementById('saluda').addEventListener('click', () =>{
+
+const nom = document.getElementById('nom').value
+
+function saludar(name, trucada) {
+    setTimeout(() => {
+        trucada(name)
+    }, 2000);
+}
+
+function trucar(nom) {
+    document.getElementById('result3').innerHTML = `Hola, ${nom}`
+}
+saludar(nom, trucar);
+});
+
+
+//Exercici 4
+
+const arrayNms = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+function processarElements(array, callba) {
+    for (let i = 0; i < array.length; i++){
+        callba(array[i])
+    }
+}
+
+function callbac(element) {
+    console.log(element)
+}
+
+processarElements(arrayNms, callbac);
+
+
+//Exercici 5
+
+document.getElementById('majuscules').addEventListener('click', () =>{
+
+    const paraulaUser = document.getElementById('paraula').value
+
+    function processarCadena(string, call) {
+        call(string)
+    }
+
+    function call(word) {
+        let paraulaMaj = word.toUpperCase()
+        document.getElementById('result4').innerHTML = paraulaMaj
+    }
+
+    processarCadena(paraulaUser, call)
+});
--- src/js/ex6.js
+++ src/js/ex6.js
@@ -1,1 +1,56 @@
 'use strict'
+
+const arrNoms = ['Anna', 'Bernat', 'Clara'];
+const arrNums = [1, 2, 3, 4, 5, 6]
+
+//Exercici 1
+console.log("Exercici 1:")
+arrNoms.forEach((noms) => console.log(noms))
+
+//Exercici 2
+
+console.log("Exercici 2")
+for (const names of arrNoms) {
+    console.log(names);
+  }
+
+//Exercici 3
+
+function parells() {
+    const parell = arrNums.filter((num) => (num % 2 == 0));
+    document.getElementById('result').innerHTML = parell;
+}
+
+
+//Exercici 4
+console.log("Exercici 4")
+let obj = { 
+    nom: 'Ona', 
+    edat: 25, 
+    ciutat: 'Barcelona' 
+};
+
+for (const propietat in obj) {
+    console.log(`obj.${propietat} = ${obj[propietat]}`);
+  }
+
+//Exercici 5
+
+console.log("Exercici 5")
+for (const nums of arrNums) {
+    if (nums > 5) {
+        break;
+    } else {
+        console.log(nums)
+    }
+  }
+
+  //Exercici 6
+  console.log("Exercici 6")
+
+  let index = 0;
+
+  for (const noms of arrNoms) {
+    console.log(index, noms);
+    index++;
+  }
